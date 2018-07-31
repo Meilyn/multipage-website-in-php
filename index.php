a40c653b804be5ce84580ebedd8cc02b638364fd
Meilyn <[andrade.meiyn@gmail.com]>
2018-07-31 14:27:29 +0200

    V.0.0.2

--- index.php
+++ index.php
@@ -4,14 +4,24 @@
   
   <body>
   	
-
+	<!-- COVER -->
    <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark" id="jumbo-base">
         <div class="col-md-6 px-0">
-          <h1 class="display-4 font-italic">Senoah</h1>
+          <h1 class="display-4 font-italic" id="Senoah">SENOAH</h1>
           <p class="lead my-3">Senoah est un service gratuit d’accompagnement des seniors en matière d’habitats et un observatoire des lieux de vie pour ainés</p>
-          <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Plus Info...</a></p>
+		<button type="button" class="btn btn-outline-light btn-lg">En savoir plus</button>
         </div>
-    </div> 
+    </div>
+    <!-- FEATURES  -->
+    <div class="container">
+    	<div class="media">
+  			<video src="Assets/images/SENOAH-SD.mp4" controls="auto"></video>
+  			<div class="media-body">
+		   		<h5 class="mt-0">Découvrez notre capsule vidéo !</h5><hr><br>
+		    		Senoah veut sensibiliser les seniors à l’importance d’envisager aujourd’hui son chez-soi de demain. Pour ce faire, l’asbl a décidé de donner la parole aux personnes qui avancent en âge. Découvrez notre capsule vidéo et le message que veulent faire passer les trois personnes interrogées sur leur chez-soi de demain.
+  			</div>
+		</div>
+	</div>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
