fa5e9cb4da51bd9e67125ab7707d30b24bf6721d
Cedric-Fournier <cedricfournier.pro@gmail.com>
2018-07-31 11:49:17 +0200

    Add Branch Dev

--- habitat.php
+++ habitat.php
@@ -2,8 +2,14 @@
 
 <?php include('Partials/menu.php') ?>
 
-<div class="container">
+<div class="container-fluid">
+  <div class="container">
+    <div class="row">
 
+      <!-- CONTENT -->
+
+    </div>
+  </div>
 <div>
 
 <?php include('Partials/footer.php') ?>
