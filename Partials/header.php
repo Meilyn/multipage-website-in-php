c161fb8da9be78f02d5cee25bec7f6565e2a5266
Cedric-Fournier <cedricfournier.pro@gmail.com>
2018-07-31 12:27:51 +0200

    Advanced contact BackEnd PHP

--- Partials/header.php
+++ Partials/header.php
@@ -5,9 +5,7 @@
     <meta name="Description" content="a chercher">
     <link rel="stylesheet" href="Assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="Assets/css/style.css">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="assets/css/bootstrap.min.js"></script>
+    <script src="Assets/css/bootstrap.min.js"></script>
     <title>Senoah - Service d'accompagnement des seniors. ASBL</title>
   </head>
   <body>
--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <?php include('Partials/header.php') ?>
 <?php include('Partials/menu.php') ?>
 
-  
-  <body>
-  	
+<!-- <div class="container-fluid">
+  <div class="container">
+    <div class="row"> -->
 
-   <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark" id="jumbo-base">
+      <div class="jumbotron p-3 p-md-5 text-white rounded bg-dark" id="jumbo-base">
         <div class="col-md-6 px-0">
           <h1 class="display-4 font-italic">Senoah</h1>
           <p class="lead my-3">Senoah est un service gratuit d’accompagnement des seniors en matière d’habitats et un observatoire des lieux de vie pour ainés</p>
           <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Plus Info...</a></p>
         </div>
-    </div> 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
-	<?php include('Partials/footer.php') ?>
-  </body>
+      </div>
+
+    <!-- </div>
+  </div>
+<div> -->
+
+<?php include('Partials/footer.php') ?>
