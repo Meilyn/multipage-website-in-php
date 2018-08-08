bee64b5a53ff36caab87197643b3945b07188109
Meilyn <[andrade.meiyn@gmail.com]>
2018-08-08 10:15:12 +0200

    mdm

--- accompagnement.php
+++
@@ -1,9 +0,0 @@
-<?php include('Partials/header.php') ?>
-
-<?php include('Partials/menu.php') ?>
-
-<div class="container">
-
-<div>
-
-<?php include('Partials/footer.php') ?>
--- habitat.php
+++
@@ -1,6 +0,0 @@
-<?php include('Partials/header.php') ?>
-
-<?php include('Partials/menu.php') ?>
-
-
-<?php include('Partials/footer.php') ?>
