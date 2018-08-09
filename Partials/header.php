05af50c8c8093bbf751fab98b0574e3ecc3f60e8
Meilyn <[andrade.meiyn@gmail.com]>
2018-08-09 14:17:29 +0200

    V.0.0.4

---
+++ Partials/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="Meilyn" content="">
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="Assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="Assets/css/style.css">
+    <link href="Assets/css/agency.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="assets/css/bootstrap.min.js"></script>
+    <title>Senoah - Service d'accompagnement des seniors. ASBL</title>
+  </head>
+  <body>
+  
---
+++ contact.php
@@ -0,0 +1,7 @@
+<?php include('./Function/mail.php'); ?>
+
+<?php include('Partials/menu.php'); ?>
+
+<?php print $file->saveHTML(); ?>
+
+<?php include('Partials/footer.php'); ?>
