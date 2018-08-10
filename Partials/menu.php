4556cda19957b0cfb05b74f43d2c60e500011e15
Meilyn <[andrade.meiyn@gmail.com]>
2018-08-10 14:20:05 +0200

    Heroku

--- Partials/menu.php
+++ Partials/menu.php
@@ -2,8 +2,8 @@
  <nav class="site-header sticky-top py-1">
       <div class="container d-flex flex-column flex-md-row justify-content-between">
         <a class="py-2 d-none d-md-inline-block active" href="#"><span class="logo">SEN</span><span class="oah">OAH</span></a>
-        <a class="py-2 d-none d-md-inline-block" href="#">ACCUEIL</a>
+        <a class="py-2 d-none d-md-inline-block" href="index.php">ACCUEIL</a>
         <a class="py-2 d-none d-md-inline-block" href="#">A PROPOS</a>
-        <a class="py-2 d-none d-md-inline-block" href="#">CONTACTEZ NOUS</a>
+        <a class="py-2 d-none d-md-inline-block" href="contact.php">CONTACTEZ NOUS</a>
       </div>
   </nav>
